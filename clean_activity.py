51179eff1b52bd1898ab1f629f1fd180019230fd
Mohamed Shaheedullah <conifer567@outlook.com>
2024-03-05 19:04:52 +0000

    activities day 2 completed

--- clean_activity.py
+++ clean_activity.py
@@ -2,33 +2,44 @@ import pandas as pd
 import openpyxl
 df = pd.read_excel("cleaning_activity.xlsx")
 
-df.info()
+# df.info()
 
 
 df = df.drop(columns=["Till ID", "Unnamed: 0"])
 
 df = df.dropna(how="any")
+df = df.drop_duplicates()
+
+
+## question 1 
+def staff_to_lowercase(employee):
+    employee = str(employee)
+    return employee.lower()
+
+df["Staff"] = df["Staff"].apply(staff_to_lowercase)
 
 df = df.set_index("Transaction ID")
 
-# df["Transaction ID"]
 
-# df = df.drop(df[df['Transaction ID'] = 56].index)
-
+## delete outlier
 df = df.drop(56)
 
-
+## find staff MVP (by cost)
 grp = df.groupby("Staff").sum("Cost")
 
-# print(df)
+new_df = grp.sort_values(by = ['Cost'], ascending=[False])
 
-print(grp)
 
-# print(df)
+print(f" Staff MVP by cost {new_df.iloc[0]}")
+
+
+## find average cost per transaction
 
 mean_per_trans = (df["Cost"].mean())
 
 print(f"Average price per transaction is {round(mean_per_trans, 2)} ")
+
+## explode basket
 
 def remove_punctuation(basket):
     basket = str(basket)
@@ -47,5 +58,7 @@ df["Basket"] = df["Basket"].apply(split_basket)
 
 df = df.explode("Basket", ignore_index=False)
 
+# print(df["Basket"].value_counts(ascending=True) prints whole lists
 
-print(df["Basket"].value_counts())
+print("Least popular item")
+print(df["Basket"].value_counts(ascending=True).index[0])
